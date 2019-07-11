7de6f962f1f5bc3a4697446cc2d6921afbc7a061
Ariana Bray <aribray@gmail.com>
2019-07-11 16:20:35 -0700

    refactored permissions request

--- app/src/main/java/com/example/sightsee/MapsActivity.java
+++ app/src/main/java/com/example/sightsee/MapsActivity.java
@@ -5,59 +5,124 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.kishan.askpermission.AskPermission;
+import com.kishan.askpermission.ErrorCallback;
+import com.kishan.askpermission.PermissionCallback;
+import com.kishan.askpermission.PermissionInterface;
 
 
 public class MapsActivity extends FragmentActivity
-        implements GoogleMap.OnMyLocationButtonClickListener,
+        implements GoogleMap.OnMyLocationButtonClickListener, PermissionCallback, ErrorCallback,
         GoogleMap.OnMyLocationClickListener,
         OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
 
-    private static final int REQUEST_LOCATION = 234;
+    private static final int REQUEST_PERMISSIONS = 20;
+
+//    private static final int REQUEST_LOCATION = 234;
+
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+//        setContentView(R.layout.activity_maps);
+//
+//        SupportMapFragment mMap =
+//                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+//        mMap.getMapAsync(this);
+
+        reqPermission();
+    }
+
+    private void reqPermission() {
+        new AskPermission.Builder(this).setPermissions(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                .setCallback(this)
+                .setErrorCallback(this)
+                .request(REQUEST_PERMISSIONS);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode) {
+        Toast.makeText(this, "Permissions Received.", Toast.LENGTH_LONG).show();
+//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
         setContentView(R.layout.activity_maps);
 
-        SupportMapFragment mapFragment =
+        SupportMapFragment mMap =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mMap.getMapAsync(this);
     }
+
+    @Override
+    public void onPermissionsDenied(int requestCode) {
+        Toast.makeText(this, "Permissions Denied.", Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public void onShowRationalDialog(final PermissionInterface permissionInterface, int requestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("We need location permissions for this app.");
+        builder.setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                permissionInterface.onDialogShown();
+            }
+        });
+        builder.setNegativeButton(R.string.btn_cancel, null);
+        builder.show();
+    }
+
+    @Override
+    public void onShowSettings(final PermissionInterface permissionInterface, int requestCode) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("We need location permissions for this app. Open setting screen?");
+        builder.setPositiveButton(R.string.btn_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                permissionInterface.onSettingsShown();
+            }
+        });
+        builder.setNegativeButton(R.string.btn_cancel, null);
+        builder.show();
+    }
+
 
     @Override
     public void onMapReady(GoogleMap map) {
         mMap = map;
-        // TODO: Before enabling the My Location layer, you must request
-        // location permission from the user. This sample does not include
-        // a request for location permission.
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Check Permissions Now
-            ActivityCompat.requestPermissions(this,
-                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_LOCATION);
-        } else {
-            // permission has been granted, continue as usual
-            mMap.setMyLocationEnabled(true);
-            mMap.setOnMyLocationButtonClickListener(this);
-            mMap.setOnMyLocationClickListener(this);
-
-        }
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
 
     }
 
